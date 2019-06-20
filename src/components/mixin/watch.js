5f4c9eed879f70253a029a69c2aa12169803f420
kenotiai <kenotiai@gmail.com>
2019-06-20 09:25:23 +0900

    微修正

--- src/components/mixin/watch.js
+++ src/components/mixin/watch.js
@@ -8,8 +8,8 @@ import Scatter from '../../chart/scatter'
 import ScatterEstat from '../../chart/scatter-estat'
 import Histogram from '../../chart/histogram'
 import Time from '../../chart/time'
-import maps77 from '../../chart/maps77'
-import rank from '../../chart/rank'
+import Maps77 from '../../chart/maps77'
+import Rank from '../../chart/rank'
 import Time2 from '../../chart/time2'
 export default {
   name: 'watch',
@@ -33,24 +33,73 @@ export default {
     s_rightStatEstatCity () { return this.$store.state.statList.rightStatEstatCity },
   },
   watch: {
+    // 宮崎県市町村用-------------------------------------------------------------------------
     s_leftStat: {
       handler: function(val) {
+        // bubble.jsだけにスライダーの詳細を設定するコードが書かれている。
+        // 並びはなんでもいい。
         Bubble(val, '#left-bubble-miyazaki-city');
-        Pie(val, '#left-pie-miyazaki-city');
-        Histogram(val, '#left-histogram-miyazaki-city');
         Bar(val, this.s_rightStat, '#left-bar-miyazaki-city');
+        Rank(val, '#left-rank-miyazaki-city');
         Map(val, '#left-map-miyazaki-city');
-        rank(val, '#left-rank-miyazaki-city');
+        Pie(val, '#left-pie-miyazaki-city');
         Tree(val, '#left-tree-miyazaki-city');
+        Histogram(val, '#left-histogram-miyazaki-city');
       },
       deep: true,
     },
+    // 宮崎県散布図用-------------------------------------------------------------------------
     s_rightStat: {
       handler: function() {
         Scatter(this.s_leftStat, this.s_rightStat);
       },
       deep: true,
     },
+    // 全国都道府県用。散布図も---------------------------------------------------------------
+    s_leftStatEstatPref: {
+      handler: function (val) {
+        Bubble(val, '#left-bubble-pref');
+        Bar(val, this.s_rightStat, '#left-bar-pref');
+        Rank(val, '#left-rank-pref');
+        Map(val, '#left-map-pref');
+        Maps77(val, '#left-map77-pref');
+        Pie(val, '#left-pie-pref');
+        Tree(val, '#left-tree-pref');
+        Histogram(val, '#left-histogram-pref');
+        Time2(val, '#left-time-pref');
+        ScatterEstat(this.s_leftStatEstatPref, this.s_rightStatEstatPref, 'pref', '#left-scatterPref');
+      },
+      deep: true,
+    },
+    // 全国都道府県散布図用------------------------------------------------------------------
+    s_rightStatEstatPref: {
+      handler: function () {
+        ScatterEstat(this.s_leftStatEstatPref, this.s_rightStatEstatPref, 'pref', '#left-scatterPref');
+      },
+      deep: true,
+    },
+    // 全国市町村用。散布図も-----------------------------------------------------------------
+    s_leftStatEstatCity: {
+      handler: function (val) {
+        Bubble(val, '#left-bubble-city');
+        Bar(val, this.s_rightStat, '#left-bar-city');
+        Rank(val, '#left-rank-city');
+        Map(val, '#left-map-city');
+        Pie(val, '#left-pie-city');
+        Histogram(val, '#left-histogram-city');
+        Time2(val, '#left-time-city');
+        ScatterEstat(this.s_leftStatEstatCity, this.s_rightStatEstatCity, 'city', '#left-scatterCity');
+      },
+      deep: true,
+    },
+    // 全国市町村散布図用--------------------------------------------------------------------
+    s_rightStatEstatCity: {
+      handler: function () {
+        ScatterEstat(this.s_leftStatEstatCity, this.s_rightStatEstatCity, 'city', '#left-scatterCity');
+      },
+      deep: true,
+    },
+    // 時系列。宮崎県用------------------------------------------------------------------------
     s_leftStatTime: {
       handler: function(val) {
         Time(val, '#left-time');
@@ -58,55 +107,17 @@ export default {
       },
       deep: true,
     },
+    // 時系列。全国都道府県用-----------------------------------------------------------------
     s_leftStatTimePref: {
       handler: function(val) {
         Time(val, '#left-timePref');
       },
       deep: true,
     },
+    // 時系列。全国市町村用-------------------------------------------------------------------
     s_leftStatTimeCity: {
       handler: function(val) {
         Time(val, '#left-timeCity');
-      },
-      deep: true,
-    },
-    s_leftStatEstatPref: {
-      handler: function (val) {
-        Bubble(val, '#left-bubble-pref');
-        Pie(val, '#left-pie-pref');
-        Histogram(val, '#left-histogram-pref');
-        Bar(val, this.s_rightStat, '#left-bar-pref');
-        Map(val, '#left-map-pref');
-        rank(val, '#left-rank-pref');
-        Tree(val, '#left-tree-pref');
-        maps77(val, '#left-map77-pref');
-        Time2(val, '#left-time-pref');
-        ScatterEstat(this.s_leftStatEstatPref, this.s_rightStatEstatPref, 'pref', '#left-scatterPref');
-      },
-      deep: true,
-    },
-    s_rightStatEstatPref: {
-      handler: function () {
-        ScatterEstat(this.s_leftStatEstatPref, this.s_rightStatEstatPref, 'pref', '#left-scatterPref');
-      },
-      deep: true,
-    },
-    s_leftStatEstatCity: {
-      handler: function (val) {
-        Bubble(val, '#left-bubble-city');
-        Pie(val, '#left-pie-city');
-        Histogram(val, '#left-histogram-city');
-        Bar(val, this.s_rightStat, '#left-bar-city');
-        Map(val, '#left-map-city');
-        rank(val, '#left-rank-city');
-        Time2(val, '#left-time-city');
-        ScatterEstat(this.s_leftStatEstatCity, this.s_rightStatEstatCity, 'city', '#left-scatterCity');
-      },
-      deep: true,
-    },
-    s_rightStatEstatCity: {
-      handler: function () {
-        ScatterEstat(this.s_leftStatEstatCity, this.s_rightStatEstatCity, 'city', '#left-scatterCity');
       },
       deep: true,
     },
