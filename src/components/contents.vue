dc5ec9ebdcccfa65124c495e5bc863968218a08d
kenotiai <kenotiai@gmail.com>
2019-06-20 09:24:55 +0900

    起動時のアニメを止めた。

--- src/components/contents.vue
+++ src/components/contents.vue
@@ -1,9 +1,7 @@
-id="year-range-ticks-pref"とid="year-range-ticks-city"は
-bubble.jsでtickを引いている。
 <template>
     <div id="contents">
         <div id="left-chart-div">
-            <!--スライダー-->
+            <!--スライダー。細かな設定はbubble.jsでデータを読み込んだときにしている。-->
             <div  :id='el.id' v-for="el in rangeDiv" :key="el.id" v-show="s_activeIndex===el.statType">
                 <span class="year-range-text"></span>
                 <div class="year-range-div">
@@ -16,7 +14,7 @@ bubble.jsでtickを引いている。
             </div>
             <!--グラフのダイアログー-->
             <draggable v-model="s_leftDivList" handle=".chart-div-handle">
-                <transition-group appear>
+                <transition-group>
                     <div :id="'left-' + el.divId" :class="'laft-chart ' +  el.class" v-for="el in s_leftDivList" :key="el.order" v-show="el.show"
                          v-loading="s_chartDivLoading"
                          element-loading-background="rgba(0, 0, 0, 0)"
